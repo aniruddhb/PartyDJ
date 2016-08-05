8ac88688202d0e64b33c7f5a8fbaaa65465e6e7e
Aniruddh Bharadwaj <aniruddh@usc.edu>
2016-08-04 19:34:21 -0700

    integrated Facebook login, twitter up next

--- PartyDJ/LoginViewController.swift
+++ PartyDJ/LoginViewController.swift
@@ -22,7 +22,37 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func facebookButtonDidTouch(sender: UIButton) {
+        // declare login manager to handle login
+        let facebookLoginManager: FBSDKLoginManager = FBSDKLoginManager()
+        
+        // declare set of login permissions
+        let facebookLoginPermissions: [AnyObject]! = ["public_profile", "email", "user_friends"]
+        
+        // request login with manager
+        facebookLoginManager.logInWithReadPermissions(facebookLoginPermissions, fromViewController: self) { (result: FBSDKLoginManagerLoginResult!, error: NSError!) in
+            // depending on result and error, present alert to user or segue to next view controller
+            if error != nil {
+                // configure and show login alert error to user
+                let alert: UIAlertController = UIAlertController(title: "Login Error", message: "There was an error logging into Facebook. Please try again later :(", preferredStyle: .Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            } else if result.isCancelled {
+                // configure and show cancel alert error to user
+                let alert: UIAlertController = UIAlertController(title: "Login Cancelled", message: "The login action was cancelled. I'm sad now :(", preferredStyle: .Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+            } else {
+                // prepare segue (for now, print success)
+                print("success")
+            }
+        }
+    }
+    
+    @IBAction func twitterButtonDidTouch(sender: UIButton) {
+    
+    }
+    
     /*
     // MARK: - Navigation
 
